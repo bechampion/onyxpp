605ab5ba0631ce77a3f8b85ad2ff7f3268649be6
jegarcia <garciaj.uk@gmail.com>
2016-08-03 10:20:02 +0100

    Some minor changes to prep.sh

--- puppet/prep.sh
+++ puppet/prep.sh
@@ -1,7 +1,6 @@
 sudo apt-get install puppet &&
 sudo gem install librarian-puppet &&
 cd /vagrant/puppet/
-sudo librarian-puppet install 
+sudo librarian-puppet install &&
 sudo curl -sSL https://rvm.io/mpapis.asc | gpg --import -
-#puppet apply manifests/ --modulepath=modules/ --hiera_config=hiera.yaml 
 
